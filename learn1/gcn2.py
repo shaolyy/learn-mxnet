9d111969e8b06d61d4465480bef877ba41f8c4c2
shaolyy <shao8@outlook.com>
2019-12-06 22:59:22 +0800

    add gcn2_model_1

--- learn1/gcn2.py
+++ learn1/gcn2.py
@@ -1,63 +1,162 @@
-class SpectralRule(HybridBlock):
-    def __init__(self,
-                 A, in_units, out_units,
-                 activation, **kwargs):
-        super().__init__(**kwargs)        
-        
+from networkx import read_edgelist, set_node_attributes
+from collections import namedtuple
+from pandas import read_csv, Series
+import numpy as np
+
+import networkx as nx
+from mxnet.ndarray import array
+from mxnet import ndarray as nd, initializer, autograd
+from mxnet.gluon import nn, loss as gloss
+from mxnet import gluon
+
+DataSet = namedtuple('DataSet', field_names=[
+                  'X_train', 'y_train', 'X_test', 'y_test', 'network'])
+network = read_edgelist('./data/karate.edgelist', nodetype=int)
+attributes = read_csv('./data/karate.attributes.csv', index_col=['node'])
+
+for attribute in attributes.columns.values:
+    print(attribute)
+    set_node_attributes(network, values=Series(attributes[attribute],
+                                               index=attributes.index).to_dict(),
+                        name=attribute)
+
+Series(attributes[attribute],index=attributes.index).to_dict()
+
+for attribute in attributes.columns.values:
+    set_node_attributes(
+        network,
+        values=Series(attributes[attribute],
+                        index=attributes.index).to_dict(),
+        name=attribute
+    )
+
+
+attributes
+X_train, y_train = map(np.array, zip(*[
+    ([node], data['role'] == 'Administrator') 
+    for node, data in network.nodes(data=True)
+    if data['role'] in {'Administrator', 'Instructor'}
+]))
+
+X_test, y_test = map(np.array, zip(*[
+    ([node], data['community'] == 'Administrator') 
+    for node, data in network.nodes(data=True)
+    if data['role'] == 'Member' 
+]))
+
+zkc = DataSet(X_train, y_train, X_test, y_test, network)
+
+A = nx.to_numpy_matrix(zkc.network)
+A = nd.array(A)
+
+X_train = zkc.X_train.flatten()
+y_train = zkc.y_train
+X_test = zkc.X_test.flatten()
+y_test = zkc.y_test
+
+y_test
+
+class SpectralRule(nn.HybridBlock):
+    def __init__(self, A, in_units, out_units, activation='relu', **kwargs):
+        super().__init__(**kwargs)
         I = nd.eye(*A.shape)
-        A_hat = A.copy() + I        
+        A_hat = A.copy() + I
         D = nd.sum(A_hat, axis=0)
         D_inv = D**-0.5
-        D_inv = nd.diag(D_inv)        
-        
+        D_inv = nd.diag(D_inv)
         A_hat = D_inv * A_hat * D_inv
-        
         self.in_units, self.out_units = in_units, out_units
-        
         with self.name_scope():
             self.A_hat = self.params.get_constant('A_hat', A_hat)
-            self.W = self.params.get(
-                'W', shape=(self.in_units, self.out_units)
-            )
-            if activation == 'ident':
+            self.W = self.params.get('W', shape=(self.in_units, self.out_units))
+            if activation == 'identity':
                 self.activation = lambda X: X
             else:
-                self.activation = Activation(activation)    
-
-	def hybrid_forward(self, F, X, A_hat, W):
+                self.activation = nn.Activation(activation)
+        
+    def hybrid_forward(self, F, X, A_hat, W):
         aggregate = F.dot(A_hat, X)
-        propagate = self.activation(
-            F.dot(aggregate, W))
+        propagate = self.activation(F.dot(aggregate, W))
         return propagate
 
+class LogisticRegressor(nn.HybridBlock):
+    def __init__(self, in_units, **kwargs):
+        super().__init__(**kwargs)
+        with self.name_scope():
+            self.w = self.params.get('w', shape=(1, in_units))
+            self.b = self.params.get('b', shape=(1, 1))
+        
+    def hybrid_forward(self, F, X, w, b):
+        b = F.broadcast_axes(b, axis=(0, 1), size=(34, 1))
+        y = F.dot(X, w, transpose_b=True) + b
+
+        return F.sigmoid(y)
+
+def build_features(A, X):
+    hidden_layer_specs = [(4, 'tanh'), (2, 'tanh')]
+    in_units = X.shape[1]
+    features = nn.HybridSequential()
+    with features.name_scope():
+        for i, (layer_size, activation_func) in enumerate(hidden_layer_specs):
+            layer = SpectralRule(A, in_units=in_units, out_units=layer_size,
+                                 activation=activation_func)
+            features.add(layer)
+            in_units = layer_size
+    return features, in_units
+
 def build_model(A, X):
-    model = HybridSequential()    
+    model = nn.HybridSequential()
+    in_units = X.shape[1]
     
     with model.name_scope():
-        features = build_features(A, X)
-        model.add(features)        
-        classifier = LogisticRegressor()
-        model.add(classifier)        
-        model.initialize(Uniform(1))    
-      
+        features, out_units = build_features(A, X)
+        model.add(features)
+
+        classifier = LogisticRegressor(out_units)
+        model.add(classifier)
+    model.hybridize()
+    model.initialize(initializer.Uniform(1))
     return model, features
 
+X_1 = I = nd.eye(*A.shape)
+model_1, features_1 = build_model(A, X_1)
+model_1(X_1)
+
 def train(model, features, X, X_train, y_train, epochs):
-    cross_entropy = SigmoidBinaryCrossEntropyLoss(from_sigmoid=True)
-    trainer = Trainer(
-        model.collect_params(), 'sgd',
-        {'learning_rate': 0.001, 'momentum': 1})    
-    
-    feature_representations = [features(X).asnumpy()]    
+    cross_entropy = gloss.SigmoidBinaryCrossEntropyLoss(from_sigmoid=True)
+    trainer = gluon.Trainer(model.collect_params(), 'sgd', 
+                            {'learning_rate':0.001, 'momentum':1})
+    feature_representations = [features(X).asnumpy()]
 
-	for e in range(1, epochs + 1):
+    for e in range(1, epochs+1):
+        cum_loss = 0
+        cum_preds = []
+
         for i, x in enumerate(X_train):
-            y = array(y_train)[i]
+            y = nd.array(y_train)[i]
             with autograd.record():
-                pred = model(X)[x] # Get prediction for sample x
-                loss = cross_entropy(pred, y)
+                preds = model(X)[x]
+                loss = cross_entropy(preds, y)
             loss.backward()
-            trainer.step(1)        
-		feature_representations.append(features(X).asnumpy())    
+            trainer.step(1)
+            cum_loss += loss.asscalar()
+            cum_preds += [preds.asscalar]
 
-	return feature_representations
+        feature_representations.append(features(X).asnumpy())
+
+        if (e % (epochs // 10)) == 0:
+            print(f"Eopch {e}/ {epochs} -- Loss: {cum_loss: .4f}")
+            print(cum_preds)
+    return feature_representations
+
+def predict(model, X, nodes):
+    preds = model(X)[nodes].asnumpy().flatten()
+    return np.where(preds >= 0.5, 1, 0)
+
+from sklearn.metrics import classification_report
+feature_representations_1 = train(model_1, features_1, X_1, X_train, y_train,
+                                    epochs=5000)
+y_pred_1 = predict(model_1, X_1, X_test)
+
+print(classification_report(y_test, y_pred_1))
+
--- learn1/notebook.py
+++ learn1/notebook.py
@@ -10,6 +10,19 @@
 # In[1]:
 
 
+from sklearn.metrics import classification_report
+import numpy as np
+from mxnet.ndarray import sum as ndsum
+from mxnet.gluon import Trainer
+from mxnet import autograd
+from mxnet.gluon.loss import SigmoidBinaryCrossEntropyLoss
+from mxnet.initializer import One, Uniform, Xavier
+from mxnet.ndarray import array
+from mxnet.gluon.nn import HybridSequential, Activation
+from mxnet.gluon.nn import Activation
+from mxnet.gluon import HybridBlock
+import mxnet.ndarray as nd
+from networkx import to_numpy_matrix, degree_centrality, betweenness_centrality, shortest_path_length
 from collections import namedtuple
 from networkx import read_edgelist, set_node_attributes
 from pandas import read_csv, Series
@@ -20,21 +33,17 @@ DataSet = namedtuple(
     field_names=['X_train', 'y_train', 'X_test', 'y_test', 'network']
 )
 
-def load_karate_club():
-    network = read_edgelist(
-        'karate.edgelist',
-        nodetype=int)
 
-    attributes = read_csv(
-        'karate.attributes.csv',
-        index_col=['node'])
+def load_karate_club():
+    network = read_edgelist('./data/karate.edgelist', nodetype=int)
+
+    attributes = read_csv('./data/karate.attributes.csv', index_col=['node'])
 
     for attribute in attributes.columns.values:
         set_node_attributes(
             network,
-            values=Series(
-                attributes[attribute],
-                index=attributes.index).to_dict(),
+            values=Series(attributes[attribute],
+                          index=attributes.index).to_dict(),
             name=attribute
         )
 
@@ -48,7 +57,7 @@ def load_karate_club():
         for node, data in network.nodes(data=True)
         if data['role'] == 'Member'
     ]))
-    
+
     return DataSet(
         X_train, y_train,
         X_test, y_test,
@@ -57,9 +66,6 @@ def load_karate_club():
 
 # In[ ]:
 
-
-from networkx import to_numpy_matrix, degree_centrality, betweenness_centrality, shortest_path_length
-import mxnet.ndarray as nd
 
 zkc = load_karate_club()
 
@@ -77,10 +83,6 @@ y_test = zkc.y_test
 # In[ ]:
 
 
-from mxnet.gluon import HybridBlock
-from mxnet.gluon.nn import Activation
-import mxnet.ndarray as nd
-
 class SpectralRule(HybridBlock):
     def __init__(self, A, in_units, out_units, activation='relu', **kwargs):
         super().__init__(**kwargs)
@@ -92,9 +94,9 @@ class SpectralRule(HybridBlock):
         D_inv = nd.diag(D_inv)
 
         A_hat = D_inv * A_hat * D_inv
-        
+
         self.in_units, self.out_units = in_units, out_units
-        
+
         with self.name_scope():
             self.A_hat = self.params.get_constant('A_hat', A_hat)
             self.W = self.params.get(
@@ -129,7 +131,7 @@ class LogisticRegressor(HybridBlock):
 
     def hybrid_forward(self, F, X, w, b):
         # Change shape of b to comply with MXnet addition API
-        b = F.broadcast_axis(b, axis=(0,1), size=(34, 1))
+        b = F.broadcast_axis(b, axis=(0, 1), size=(34, 1))
         y = F.dot(X, w, transpose_b=True) + b
 
         return F.sigmoid(y)
@@ -140,30 +142,27 @@ class LogisticRegressor(HybridBlock):
 # In[ ]:
 
 
-from mxnet.gluon.nn import HybridSequential, Activation
-from mxnet.ndarray import array
-from mxnet.initializer import One, Uniform, Xavier
-from mxnet.gluon.loss import SigmoidBinaryCrossEntropyLoss
-
 def build_features(A, X):
-    hidden_layer_specs = [(4, 'tanh'), (2, 'tanh')] # Format: (units in layer, activation function)
-    in_units = in_units=X.shape[1]
-  
+    # Format: (units in layer, activation function)
+    hidden_layer_specs = [(4, 'tanh'), (2, 'tanh')]
+    in_units = in_units = X.shape[1]
+
     features = HybridSequential()
     with features.name_scope():
         for i, (layer_size, activation_func) in enumerate(hidden_layer_specs):
             layer = SpectralRule(
-                A, in_units=in_units, out_units=layer_size, 
+                A, in_units=in_units, out_units=layer_size,
                 activation=activation_func)
             features.add(layer)
 
             in_units = layer_size
     return features, in_units
 
+
 def build_model(A, X):
     model = HybridSequential()
     hidden_layer_specs = [(4, 'tanh'), (2, 'tanh')]
-    in_units = in_units=X.shape[1]
+    in_units = in_units = X.shape[1]
 
     with model.name_scope():
         features, out_units = build_features(A, X)
@@ -215,15 +214,13 @@ model_2(X_2)
 # In[ ]:
 
 
-get_ipython().run_line_magic('time', '')
-from mxnet import autograd
-from mxnet.gluon import Trainer
-from mxnet.ndarray import sum as ndsum
-import numpy as np
+# get_ipython().run_line_magic('time', '')
+
 
 def train(model, features, X, X_train, y_train, epochs):
     cross_entropy = SigmoidBinaryCrossEntropyLoss(from_sigmoid=True)
-    trainer = Trainer(model.collect_params(), 'sgd', {'learning_rate': 0.001, 'momentum': 1})
+    trainer = Trainer(model.collect_params(), 'sgd', {
+                      'learning_rate': 0.001, 'momentum': 1})
 
     feature_representations = [features(X).asnumpy()]
 
@@ -243,11 +240,12 @@ def train(model, features, X, X_train, y_train, epochs):
             cum_preds += [preds.asscalar()]
 
         feature_representations.append(features(X).asnumpy())
-            
+
         if (e % (epochs//10)) == 0:
             print(f"Epoch {e}/{epochs} -- Loss: {cum_loss: .4f}")
             print(cum_preds)
     return feature_representations
+
 
 def predict(model, X, nodes):
     preds = model(X)[nodes].asnumpy().flatten()
@@ -259,9 +257,8 @@ def predict(model, X, nodes):
 # In[ ]:
 
 
-from sklearn.metrics import classification_report
-
-feature_representations_1 = train(model_1, features_1, X_1, X_train, y_train, epochs=5000)
+feature_representations_1 = train(
+    model_1, features_1, X_1, X_train, y_train, epochs=5000)
 y_pred_1 = predict(model_1, X_1, X_test)
 print(classification_report(y_test, y_pred_1))
 
@@ -271,7 +268,7 @@ print(classification_report(y_test, y_pred_1))
 # In[ ]:
 
 
-feature_representations_2= train(model_2, features_2, X_2, X_train, y_train, epochs=250)
+feature_representations_2 = train(
+    model_2, features_2, X_2, X_train, y_train, epochs=250)
 y_pred_2 = predict(model_2, X_2, X_test)
 print(classification_report(y_test, y_pred_2))
-
